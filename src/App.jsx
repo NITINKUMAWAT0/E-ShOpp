59a65449820a84919e7ee2706ded218c6d869d37
NITINKUMAWAT0 <nitin.kumawat.jaipur.in@gmail.com>
2023-10-19 23:49:26 +0530

    react-router-dom setup completed!!

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import React from 'react';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import NewsLetter from './components/Footer/Newsletter/Newsletter';
+import Category from './components/Category/Category';
+import Home from './components/Home/Home';
+import SingleProduct from './components/SingleProduct/SingleProduct';
+import Newsletter from "./components/Footer/Newsletter/Newsletter";
+import Context from './utils/context';
+import AppContext from "./utils/context";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <AppContext>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/category:id" element={<Category/>}/>
+      <Route path="/product:id" element={<SingleProduct/>}/>
+    </Routes>
+    <Newsletter/>
+    <Footer/>
+    </AppContext>
+    </BrowserRouter>
+  )
+}
+
+export default App
