2453649ba78b965f33e3b609c5db3ac466192269
NITINKUMAWAT0 <nitin.kumawat.jaipur.in@gmail.com>
2023-10-26 09:57:08 +0530

    search product model completed!!

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,11 +1,25 @@
 import "./Search.scss";
 import { MdClose } from "react-icons/md";
+import prod from "../../../assets/products/headphone-prod-2.webp";
 const Search = ({ setShowSearch }) => {
   return (
     <div className="search-model">
       <div className="form-field">
         <input type="text" autoFocus placeholder="Search for products" />
-        <MdClose onClick={()=>setShowSearch(false)} />
+        <MdClose onClick={() => setShowSearch(false)} />
+      </div>
+      <div className="search-result-content">
+        <div className="search-results">
+          <div className="search-result-item">
+            <div className="img-container">
+              <img src={prod} alt="" />
+            </div>
+            <div className="pro-details">
+              <span className="name">Product name</span>
+              <span className="prod-description">Product details</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
